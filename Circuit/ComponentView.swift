2e2fd381a1e04d3533bc98a6eda518ece9339126
Logan Smith <logan.r.smith@biola.edu>
2017-06-27 21:17:16 -0700

    informatively colored outputs on components

--- Circuit/ComponentView.swift
+++ Circuit/ComponentView.swift
@@ -5,155 +5,7 @@ let NoConnectionColor = NSColor.red
 let ConnectionColor = NSColor.green
 let IncompleteConnectionsColor = NSColor.yellow
 let CompleteConnectionsColor = NSColor.white
-
-class ConnectorNode: SKSpriteNode {
-	private var connectingLineNodes = Dictionary<ConnectorNode, SKShapeNode>()
-	// the currently-being-dragged line we treat as being connected to self
-	private var connections = Set<ConnectorNode>()
-	private var isTouchDown = false
-	private var overrideIsTouchDown = false
-	private var previousConnection: ConnectorNode?
-	
-	enum ConnectorType {
-		case InputConnector
-		case OutputConnector
-	}
-	let connectorType: ConnectorType
-	let component: ComponentView
-	let connectorIndex: Int
-	init(component c: ComponentView, connectorIndex i: Int, connectorType type: ConnectorType) {
-		component = c
-		connectorIndex = i
-		connectorType = type
-		super.init(texture: nil, color: NSColor.blue, size: CGSize(width: 15, height: 15))
-		self.zPosition = 1.0
-		self.isUserInteractionEnabled = true
-		self.name = "connector"
-	}
-	
-	required init?(coder aDecoder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
-	
-	func updateConnectionLine(forConnection connection: ConnectorNode, newPos pos: CGPoint) {
-		guard connectorType == ConnectorType.OutputConnector else { return }
-		if let oldLineNode = connectingLineNodes[connection] {
-			oldLineNode.removeFromParent()
-		}
-		
-		let xDistance = abs(pos.x)
-		let curveAmount = xDistance / 2.0
-		var points = [
-			CGPoint.zero,
-			CGPoint(x: curveAmount, y: 0),
-			CGPoint(x: pos.x - curveAmount, y: pos.y),
-			pos,
-			]
-		let newNode = SKShapeNode(points: &points, count: points.count)
-		newNode.lineWidth = 3
-		newNode.strokeColor = NSColor(calibratedWhite: 1.0, alpha: 0.5)
-		newNode.name = "connectingLineNode"
-		newNode.isUserInteractionEnabled = false
-		newNode.zPosition = -5.0
-		addChild(newNode)
-		connectingLineNodes.updateValue(newNode, forKey: connection)
-	}
-	
-	func updateConnectionLine(forConnection connection: ConnectorNode) {
-		let newPos = self.convert(CGPoint.zero, from: connection)
-		updateConnectionLine(forConnection: connection, newPos: newPos)
-	}
-	
-	func updateAllConnectionLines() {
-		for connection in connectingLineNodes.keys {
-			updateConnectionLine(forConnection: connection)
-		}
-	}
-	
-	func severConnection(_ connection: ConnectorNode) {
-		connectingLineNodes[connection]?.removeFromParent()
-		connectingLineNodes.removeValue(forKey: connection)
-		if connections.contains(connection) {
-			connection.component.removeConnectionLine(connectorIndex: connection.connectorIndex)
-		}
-		connections.remove(connection)
-	}
-	
-	func touchDown(atPoint pos : CGPoint) {
-		if connectorType == ConnectorType.InputConnector {
-			if let existingConnection = self.component.getConnectionLine(inputIndex: self.connectorIndex) {
-				existingConnection.severConnection(self)
-				previousConnection = existingConnection
-				existingConnection.overrideIsTouchDown = true
-				existingConnection.touchDown(atPoint: convert(pos, to: existingConnection))
-				existingConnection.overrideIsTouchDown = false
-				isTouchDown = true
-			} else {
-				isTouchDown = false
-			}
-		} else {
-			if overrideIsTouchDown || self.frame.contains(self.convert(pos, to: self.parent!)) {
-				isTouchDown = true
-				updateConnectionLine(forConnection: self, newPos: pos)
-			} else {
-				isTouchDown = false
-			}
-		}
-	}
-	
-	func touchMoved(toPoint pos : CGPoint) {
-		guard isTouchDown else { return }
-		guard connectorType == ConnectorType.OutputConnector else {
-			if let previousConnection = previousConnection {
-				previousConnection.touchMoved(toPoint: convert(pos, to: previousConnection))
-			}
-			return
-		}
-		
-		updateConnectionLine(forConnection: self, newPos: pos)
-	}
-	
-	func touchUp(atPoint pos : CGPoint) {
-		guard isTouchDown else { return }
-		guard connectorType == ConnectorType.OutputConnector else {
-			if let previousConnection = previousConnection {
-				previousConnection.touchUp(atPoint: convert(pos, to: previousConnection))
-			}
-			return
-		}
-		severConnection(self)
-		var newConnection: ConnectorNode?
-		let worldPos = self.convert(pos, to: self.scene!)
-		self.scene!.enumerateChildNodes(withName: "//component/connector", using: { (node: SKNode, stop: UnsafeMutablePointer<ObjCBool>) in
-			guard node != self else { return }
-			let nodePoint = node.parent!.convert(worldPos, from: self.scene!)
-			if node.frame.contains(nodePoint) {
-				if let connector = node as? ConnectorNode, connector.connectorType == ConnectorType.InputConnector {
-					stop.initialize(to: true)
-					self.connections.insert(connector)
-					connector.component.receiveConnectionLine(node: self, inputIndex: connector.connectorIndex)
-					newConnection = connector
-				}
-			}
-		})
-		if let newConnection = newConnection {
-			updateConnectionLine(forConnection: newConnection)
-		}
-		isTouchDown = false
-	}
-	
-	override func mouseDown(with event: NSEvent) {
-		self.touchDown(atPoint: event.location(in: self))
-	}
-	
-	override func mouseDragged(with event: NSEvent) {
-		self.touchMoved(toPoint: event.location(in: self))
-	}
-	
-	override func mouseUp(with event: NSEvent) {
-		self.touchUp(atPoint: event.location(in: self))
-	}
-}
+let GenericConnectorColor = NSColor.blue
 
 class ComponentView: SKSpriteNode {
 	let component: Component
@@ -221,14 +73,15 @@ class ComponentView: SKSpriteNode {
 		fatalError("unsupported component")
 	}
 	
-	func setUpConnectorNodes() {
+    func setUpConnectorNodes(gameScene: GameScene) {
 		let createNode = {(num: Int, total: Int, edge: Int) -> Void in
 			let frac = 1.0 / CGFloat(total + 1)
 			let node = ConnectorNode(component: self,
 			                         connectorIndex: num,
 			                         connectorType: edge == 0
 										? ConnectorNode.ConnectorType.InputConnector
-										: ConnectorNode.ConnectorType.OutputConnector)
+										: ConnectorNode.ConnectorType.OutputConnector,
+			                         gameScene: gameScene)
 			node.position = CGPoint(x: CGFloat(edge) * self.size.width, y: frac * CGFloat(num+1) * self.size.height)
 			self.addChild(node)
 			self.connectors.append(node)
@@ -248,7 +101,7 @@ class ComponentView: SKSpriteNode {
 		self.isUserInteractionEnabled = true
 		self.anchorPoint = CGPoint(x: 0.0, y: 0.0)
 		self.name = "component"
-		setUpConnectorNodes()
+        setUpConnectorNodes(gameScene: gameScene)
 		self.colorBlendFactor = 1.0
 		
 		gameScene.model.circuitStateChanged.addHandler(handler: updateColor)
